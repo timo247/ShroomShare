4800fcb8ca6a68964a52e53d600e9df1c860e309
Calvin Uebelhart <uebelhartcalvin139@gmail.com>
2022-10-10 16:03:07 +0200

    add schemas

---
+++ schemas/mushroom.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose'
+const Schema = mongoose.Schema
+
+ const userSchema = new Schema ({
+    species_id: {type: Schema.Types.ObjectId, ref: 'species'}, 
+
+	user_id: {type: Schema.Types.ObjectId, ref: 'user'}, 
+
+	picture: {type : String, required:true },  
+
+	description: {type : String, required:false },
+
+    date: {type : Date, required:true, default: Date.now},
+
+    location: {
+        type : Feature,
+        geometry : {
+          type : Point,
+          coordinates: [125.6, 10.1]
+        }
+
+
+} 
+
+ })
