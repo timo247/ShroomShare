9214542e95912580ae86dda9be1daca12174dd65
Shammas44 <sebastien.traber@hotmail.com>
2022-10-17 21:35:47 +0200

    usersSeeder fixed

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,10 @@ const router = express.Router();
 router.get('/', async (req, res, next) => {
   User.find().sort('name').exec((err, users) => {
     if (err) return next(err);
-    res.send(users);
+    req.body = {};
+    req.body.message = 'Users retrieved';
+    req.body.users = users;
+    next();
   });
 });
 
@@ -23,22 +26,41 @@ router.get('/:id', (req, res, next) => {
 // Create a new user
 router.post('/', (req, res, next) => {
   const user = new User(req.body);
-  user.save((err, savedUser) => {
-    if (err) {
-      return next(err);
-    }
+  User.save((err, savedUser) => {
+    if (err) return next(err);
     res.send(savedUser);
   });
 });
 
 // Modify existing user
 router.patch('/:id', (req, res, next) => {
-  res.send('Got a response from the users route');
+  const id = req.id;
+  const params = req.params;
+  User.findByIdAndUpdate({ _id: id }, params, (err, modifiedUser) => {
+    if (err) return next(err);
+    res.send(modifiedUser);
+  });
 });
 
 // Delete an existing user
 router.delete('/:id', (req, res, next) => {
-  res.send('Got a response from the users route');
+  const id = req.params.id;
+  User.deleteOne({ _id: id }, (err, deletedUser) => {
+    if (err) return next(err);
+    res.send(deletedUser);
+  });
+});
+
+router.delete('/', (req, res, next) => {
+  User.deleteMany({}, (err, deletedUser) => {
+    if (err) return next(err);
+    res.send(deletedUser);
+  });
+});
+
+router.use((req, res, next) => {
+  const body = { ...req.body };
+  res.send(body);
 });
 
 export default router;
--- seeders/usersSeeder.js
+++ seeders/usersSeeder.js
@@ -1,27 +1,31 @@
 import User from '../schemas/user.js';
-import connexion from '../database-connector.js';
 
 const numberOfUsers = 10;
 const startIndex = 1;
 
 async function seeder(i = startIndex) {
   const user = await User.find({ username: `user${i}` });
-  if (user.length === 0) createUser(i);
-  if (i < numberOfUsers) seeder(i + 1);
+  if (user.length === 0) await createUser(i);
+  if (i < numberOfUsers) await seeder(i + 1);
 }
 
-function createUser(i) {
+async function createUser(i) {
   const user = new User({
     username: `user${i}`,
     password: `password${i}`,
     admin: i % 2 === 0,
     email: `user${i}@gmail.com`,
   });
-
-  user.save((err, updatedUser) => {
-    if (err) return console.warn(`Could not save user because: ${err.message}`);
-    return console.log('user succesfully added to db');
-  });
+  try {
+    await user.save();
+    console.log('user succesfully added to db');
+  } catch (error) {
+    console.warn(`Could not save user because: ${error}`);
+  }
+  /* user.save((err, updatedUser) => { */
+  /*   if (err) return console.warn(`Could not save user because: ${err.message}`); */
+  /*   return console.log('user succesfully added to db'); */
+  /* }); */
 }
 
 export default seeder;
