4f99d7b7b3f61e5e31af78d6e8ce9d651d772bb4
Calvin Uebelhart <uebelhartcalvin139@gmail.com>
2022-11-02 18:32:30 +0100

    Fix base64 converter

--- src/helpers/imgBase64.js
+++ src/helpers/imgBase64.js
@@ -1,29 +1,7 @@
-// export default function tobase64(url, callback){
-//     const xhr = new XMLHttpRequest();
-//     xhr.onload = function() {
-//         const reader = new FileReader();
-//         reader.onloadend= function() {
-//             callback(reader.result);
-//         }
-//         reader.readAsDataURL(xhr.response);
-//     };
-//     xhr.open('GET', url);
-//     xhr.responseType = 'blob';
-//     xhr.send();
-// }
+import fs from 'fs'
 
-
-export default function tobase64(url){
-
-    const xhr = new XMLHttpRequest();
-    xhr.onload = function() {
-        const reader = new FileReader();
-        reader.onloadend= function() {
-            return reader.result;
-        }
-        reader.readAsDataURL(xhr.response);
-    };
-    xhr.open('GET', url);
-    xhr.responseType = 'blob';
-    xhr.send();
+export default function tobase64(path){
+    let imagebase64 = fs.readFileSync(path, 'base64');
+    console.log(imagebase64);
+    return imagebase64
 }
