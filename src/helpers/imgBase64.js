1e12d9e5c2ef5401261e82e2c4a6594dc0a5552e
Calvin Uebelhart <uebelhartcalvin139@gmail.com>
2022-11-02 18:36:15 +0100

    Fix base64 converter with datainfos

--- src/helpers/imgBase64.js
+++ src/helpers/imgBase64.js
@@ -2,6 +2,6 @@ import fs from 'fs'
 
 export default function tobase64(path){
     let imagebase64 = fs.readFileSync(path, 'base64');
-
-    return imagebase64
+let imagebase64andinfos = "data:image/jpg;base64,"+imagebase64
+    return imagebase64andinfos
 }
